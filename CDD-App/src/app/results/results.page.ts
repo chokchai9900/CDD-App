67018e2c443d881317bde76dbe3c37274ed68f20
Chokchai Homsombat <chokchai@perfenterprise.com>
2021-07-04 04:04:03 +0700

    added chart.js to project

--- CDD-App/src/app/results/results.page.ts
+++ CDD-App/src/app/results/results.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild ,ElementRef} from '@angular/core';
+import { Chart ,registerables } from 'chart.js';
 
 @Component({
   selector: 'app-results',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsPage implements OnInit {
 
-  constructor() { }
+  @ViewChild("barCanvas") barCanvas: ElementRef;
+  private barChart: Chart;
+
+  constructor() {
+    Chart.register(...registerables);
+   }
 
   ngOnInit() {
+    
+  }
+  ionViewDidEnter(){
+    this.mycharts();
+  }
+
+  mycharts() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      }
+    });
   }
 
 }
