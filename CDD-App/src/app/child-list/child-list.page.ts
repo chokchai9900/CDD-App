1aa0cb5ec7102520334c6c8ff5e444558c39c976
Chokchai Homsombat <chokchai@perfenterprise.com>
2021-07-06 02:38:14 +0700

    added logic for cal date

--- CDD-App/src/app/child-list/child-list.page.ts
+++ CDD-App/src/app/child-list/child-list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { DBContextService } from '../services/dbcontext.service';
 
 @Component({
   selector: 'app-child-list',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildListPage implements OnInit {
 
-  constructor() { }
+  public ProfileData$ = Promise.resolve<any>([]) ; 
+  constructor(private service: DBContextService,private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.ProfileData$ = this.service.getProfileChild()
+    this.ProfileData$.then((it :any) =>{
+      console.log(this.ProfileData$);
+    });
+    
+    
+  }
+  getDetail(data :any){
+    let navigationExtras: NavigationExtras  = {
+      state:{
+        data: data
+      }
+    };
+    this.router.navigate(['profile'],navigationExtras)
   }
 
 }
--- CDD-App/src/app/services/dbcontext.service.ts
+++ CDD-App/src/app/services/dbcontext.service.ts
@@ -12,19 +12,55 @@ export class DBContextService {
   httpOption = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
-  private url : string = 'https://cdd-service.herokuapp.com/api/';
+  private url : string = 'https://cdd-api-net.herokuapp.com/api/';
   constructor(private httpClient: HttpClient) { }
 
+  //rate data API
   getRateData(): Promise<any>{
-    let apiUrl = this.url + "rateData/";
+    let apiUrl = this.url + "RateData/";
     return this.httpClient.get(apiUrl).toPromise();
   }
   getRateDataByID(_id: string): Promise<any>{
-    let apiUrl = this.url + "rateData/getByID/" + _id;
+    let apiUrl = this.url + "RateData/GetByID/" + _id;
     return this.httpClient.get(apiUrl).toPromise();
   }
-  getRateDataByAgeValidate(_id: string): Promise<any>{
-    let apiUrl = this.url + "rateData/getByAgeValidate/" + _id;
+  getRateDataByAgeValidate(RateValidate: string,type: string): Promise<any>{
+    let apiUrl = this.url + "RateData/GetDataByAgeAndType/" + RateValidate + type;
     return this.httpClient.get(apiUrl).toPromise();
+  }
+  //profile API
+  getProfileChild() :Promise<any>{
+    let apiUrl = this.url + "ProfileChild";
+    return this.httpClient.get(apiUrl).toPromise();
+  }
+  getProfileChildById(_id: string): Promise<any>{
+    let apiUrl = this.url + "ProfileChild/GetById" + _id;
+    return this.httpClient.get(apiUrl).toPromise();
+  }
+  createProfileChild(data:any){
+    let apiUrl = this.url + "ProfileChild/Create";
+    console.log(JSON.stringify(data));
+    return this.httpClient.post(apiUrl,data).toPromise();
+  }
+  UpdateProfileChild(data:any){
+    let apiUrl = this.url + "ProfileChild/Update?";
+    return this.httpClient.put(apiUrl,data).toPromise();
+  }
+  deleteProfileChild(id:string){
+    let apiUrl = this.url + "ProfileChild/Delete?" + id;
+    return this.httpClient.delete(apiUrl).toPromise();
+  }
+  //rate result AIP
+  getResultByID(id:string){
+    let apiUrl = this.url + "RateResult/GetByID" + id;
+    return this.httpClient.get(apiUrl).toPromise();
+  }
+  getResultChildByID(id:string){
+    let apiUrl = this.url + "RateResult/GetByChildID" + id;
+    return this.httpClient.get(apiUrl).toPromise();
+  }
+  RateChild(childId: string,age: string,rateType: string,rate :boolean){
+    let apiUrl = this.url + "RateResult/GetByChildID" + childId + age + rateType + rate;
+    return this.httpClient.put(apiUrl,null).toPromise();
   }
 }
