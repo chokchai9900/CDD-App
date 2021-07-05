1553dc7a128d4036d258c1371c8e81b66fa95d95
lali42 <lalita.su.61@ubu.ac.th>
2021-07-05 15:15:12 +0700

    edit chard

--- CDD-App/src/app/results/results.page.ts
+++ CDD-App/src/app/results/results.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit ,ViewChild ,ElementRef} from '@angular/core';
-import { Chart ,registerables } from 'chart.js';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-results',
@@ -7,75 +7,72 @@ import { Chart ,registerables } from 'chart.js';
   styleUrls: ['./results.page.scss'],
 })
 export class ResultsPage implements OnInit {
-
-  @ViewChild("barCanvas") barCanvas: ElementRef;
+  @ViewChild('barCanvas') barCanvas: ElementRef;
   private barChart: Chart;
 
   constructor() {
     Chart.register(...registerables);
-    Chart.defaults.font.family = "Kanit";
-    Chart.defaults.color="white";
-    Chart.defaults.font.size=20;
-   }
-
-  ngOnInit() {
-    
+    Chart.defaults.font.family = 'Kanit';
+    Chart.defaults.color = 'white';
+    Chart.defaults.font.size = 14;
   }
-  ionViewDidEnter(){
+
+  ngOnInit() {}
+  ionViewDidEnter() {
     this.mycharts();
   }
 
   mycharts() {
     this.barChart = new Chart(this.barCanvas.nativeElement, {
-      type: "bar",
+      type: 'bar',
       data: {
-        labels: ["GM", "FM", "RL", "EL", "PS"],
+        labels: ['GM', 'FM', 'RL', 'EL', 'PS'],
         datasets: [
           {
-            label: "# of Votes",
-            data: [1,2,3,1,4],
+            label: '',
+            data: [1, 2, 3, 1, 4],
             backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
+              '#f5b7b1',
+              '#d7bde2',
+              '#aed6f1',
+              '#a2d9ce',
+              '#f9e79f',
+              // "rgba(255, 159, 64, 0.2)"
             ],
-            borderColor: [
-              "rgba(255,99,132,1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)"
-            ],
-            borderWidth: 1
-          }
-        ]
+            // borderColor: [
+            //   "rgba(255,99,132,1)",
+            //   "rgba(54, 162, 235, 1)",
+            //   "rgba(255, 206, 86, 1)",
+            //   "rgba(75, 192, 192, 1)",
+            //   "rgba(153, 102, 255, 1)",
+            //   "rgba(255, 159, 64, 1)"
+            // ],
+            // borderWidth: 1
+          },
+        ],
       },
-      options:{
-        scales:{
-          yAxes:{
-            ticks:{
-              callback: function(label,index,labels){
+      options: {
+        scales: {
+          yAxes: {
+            ticks: {
+              callback: function (label, index, labels) {
                 switch (label) {
-                case 0:
-                  return '';
-                case 1:
+                  case 0:
+                    return '';
+                  case 1:
                     return '2 ปี 1 เดือน';
-                case 2:
+                  case 2:
                     return '2 ปี 6 เดือน';
-                case 3:
+                  case 3:
                     return '2 ปี 7 เดือน';
-                case 4:
+                  case 4:
                     return '3 ปี 1 เดือน';
                 }
-              }
-            }
-          }
-        }
-      }
+              },
+            },
+          },
+        },
+      },
     });
   }
 }
